388d63aa37fd47e04f0d69ca9c289bb6bbd641a2
Syamraj P Satheesh Kumar <125185368+syamrajsatheesh@users.noreply.github.com>
2024-08-01 10:49:03 +0200

    Experiments

--- Experiments/Extended_Clusterbased_Anomaly_Detection/Clusterer_SPARCCStream/Cluster_Density_Variation/howto_oa_cbad_051_run_ClusterDensityChangeDetector_SPARCCStream_2D.py
+++ Experiments/Extended_Clusterbased_Anomaly_Detection/Clusterer_SPARCCStream/Cluster_Density_Variation/howto_oa_cbad_051_run_ClusterDensityChangeDetector_SPARCCStream_2D.py
@@ -1,16 +1,15 @@
 ## -------------------------------------------------------------------------------------------------
 ## -- Project : MLPro - The integrative middleware framework for standardized machine learning
 ## -- Package : mlpro.test
-## -- Module  : howto_oa_cbad_021_ClusterDriftDetector_SPARCCSTream_2D.py
+## -- Module  : howto_oa_cbad_041_run_ClusterSizeChangeDetector_SPARCCStream_2D.py
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
-## -- 2024-06-19  0.0.0     SK       Creation
-## -- 2024-06-19  1.0.0     SK       Release
+## -- 2024-07-20  1.0.0     SK       Creation and Release
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 1.0.0 (2024-06-19)
+Ver. 1.0.0 (2024-07-20)
 
 
 """
@@ -21,29 +20,28 @@ from mlpro.bf.streams.streams.clouds import *
 from mlpro.bf.various import Log
 from mlpro.oa.streams import *
 from sparccstream import *
-from mlpro.oa.streams.tasks.anomalydetectors.cb_detectors.drift_detector import ClusterDriftDetector
+from mlpro.oa.streams.tasks.anomalydetectors.cb_detectors.density_change_detector import ClusterDensityChangeDetector
 
 
 
 # 1 Prepare a scenario
 class MyScenario(OAScenario):
 
-    C_NAME = 'ClusterDriftDetectorScenario'
+    C_NAME = 'ClusterDensityChangeDetector'
 
     def _setup(self, p_mode, p_ada: bool, p_visualize: bool, p_logging):
 
         # 1.1 Get MLPro benchmark stream
         stream = StreamMLProClusterGenerator(p_num_dim=2,
-                                             p_num_instances=1000,
-                                             p_num_clusters=3,
-                                             p_radii=[100],
-                                             p_velocities=[0.0],
-                                             p_change_velocities=True,
-                                             p_changed_velocities=[0.1, 0.1],
-                                             p_points_of_change_velocities=[400, 400],
-                                             p_num_clusters_for_change_velocities=2,
-                                             p_seed=12,
-                                             p_logging=p_logging)
+                                                  p_num_instances=1000,
+                                                  p_num_clusters=3,
+                                                  p_radii=[100],
+                                                  p_distribution_bias=[1, 1, 1],
+                                                  p_change_distribution_bias=True,
+                                                  p_points_of_change_distribution_bias=[600],
+                                                  p_num_clusters_for_change_distribution_bias=2,
+                                                  p_seed=12,
+                                                  p_logging=p_logging)
 
 
         # 1.2 Set up a stream workflow
@@ -70,13 +68,11 @@ class MyScenario(OAScenario):
         workflow.add_task(p_task = task_clusterer)
 
         # Anomaly Detector
-        task_anomaly_detector = ClusterDriftDetector(p_clusterer=task_clusterer,
-                                                     p_with_time_calculation=False,
-                                                     p_velocity_threshold_factor=2,
-                                                     p_initial_skip=300,
-                                                     p_buffer_size=10,
-                                                     p_visualize=p_visualize,
-                                                     p_logging=p_logging)
+        task_anomaly_detector = ClusterDensityChangeDetector(p_clusterer=task_clusterer,
+                                                             p_density_thresh_factor=1,
+                                                             p_relative_thresh=True,
+                                                             p_visualize=p_visualize,
+                                                             p_logging=p_logging)
 
         workflow.add_task(p_task=task_anomaly_detector, p_pred_tasks=[task_clusterer])
 
@@ -88,7 +84,6 @@ class MyScenario(OAScenario):
 # 2 Prepare Demo/Unit test mode
 if __name__ == '__main__':
     cycle_limit = 1000
-    #logging     = Log.C_LOG_NOTHING
     logging     = Log.C_LOG_ALL
     visualize   = True
     step_rate   = 1
@@ -107,11 +102,13 @@ myscenario = MyScenario( p_mode=Mode.C_MODE_SIM,
                                p_logging=logging )
 
 
+
+
 # 4 Reset and run own stream scenario
 myscenario.reset()
 
 if __name__ == '__main__':
-    myscenario.init_plot( p_plot_settings=PlotSettings( p_view = PlotSettings.C_VIEW_ND,
+    myscenario.init_plot( p_plot_settings=PlotSettings( p_view = PlotSettings.C_VIEW_2D,
                                                         p_step_rate = step_rate ) )
     input('\nPlease arrange all windows and press ENTER to start stream processing...')
 
@@ -141,8 +138,7 @@ for anomaly in anomalies.values():
      for x in clusters.keys():
         clusters_affected[x] = {}
         clusters_affected[x]["centroid"] = list(clusters[x].centroid.value)
-        clusters_affected[x]["size"] = clusters[x].size.value
-        clusters_affected[x]["age"] = clusters[x].age.value
+        clusters_affected[x]["density"] = clusters[x].size.value/clusters[x].size_geo.value
      
      inst = anomaly.get_instances()[-1].get_id()
      myscenario.log(Log.C_LOG_TYPE_W, 
--- Experiments/Network_Traffic_Monitoring/SPARCCStream/howto_oa_cbad_network_traffic_monitoring_SPARCCStream_2d_pca_ClusterDrift.py
+++ Experiments/Network_Traffic_Monitoring/SPARCCStream/howto_oa_cbad_network_traffic_monitoring_SPARCCStream_2d_pca_ClusterDrift.py
@@ -1,62 +1,80 @@
+
 ## -------------------------------------------------------------------------------------------------
 ## -- Project : MLPro - The integrative middleware framework for standardized machine learning
-## -- Package : mlpro.test
-## -- Module  : howto_oa_cbad_021_ClusterDriftDetector_SPARCCSTream_2D.py
+## -- Module  : howto_bf_streams_002_accessing_data_from_csv_files.py
 ## -------------------------------------------------------------------------------------------------
 ## -- History :
 ## -- yyyy-mm-dd  Ver.      Auth.    Description
-## -- 2024-06-19  0.0.0     SK       Creation
-## -- 2024-06-19  1.0.0     SK       Release
+## -- 2024-07-19  0.0.0     SK       Creation 
 ## -------------------------------------------------------------------------------------------------
 
 """
-Ver. 1.0.0 (2024-06-19)
-
+Ver. 0.0.0 (2024-07-19)
 
 """
 
-
+from datetime import datetime
 from mlpro.bf.streams.streams import *
-from mlpro.bf.streams.streams.clouds import *
-from mlpro.bf.various import Log
+from mlpro.bf.various import *
+from mlpro.bf.data import *
+from pathlib import Path
+from mlpro.bf.streams.tasks import Rearranger
 from mlpro.oa.streams import *
 from sparccstream import *
 from mlpro.oa.streams.tasks.anomalydetectors.cb_detectors.drift_detector import ClusterDriftDetector
 
 
-
-# 1 Prepare a scenario
+## -------------------------------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 class MyScenario(OAScenario):
 
-    C_NAME = 'ClusterDriftDetectorScenario'
+    C_NAME = '2DExperiment'
 
+## -------------------------------------------------------------------------------------------------
     def _setup(self, p_mode, p_ada: bool, p_visualize: bool, p_logging):
 
-        # 1.1 Get MLPro benchmark stream
-        stream = StreamMLProClusterGenerator(p_num_dim=2,
-                                             p_num_instances=1000,
-                                             p_num_clusters=3,
-                                             p_radii=[100],
-                                             p_velocities=[0.0],
-                                             p_change_velocities=True,
-                                             p_changed_velocities=[0.1, 0.1],
-                                             p_points_of_change_velocities=[400, 400],
-                                             p_num_clusters_for_change_velocities=2,
-                                             p_seed=12,
-                                             p_logging=p_logging)
+        # 1 Prepare the stream from CSV
+        path = str(Path.home())
+        
+        # 2 Instantiate Stream
+        stream = StreamMLProCSV(p_logging=p_logging,
+                            p_path_load=path,
+                            p_csv_filename="final_transformed_values_23_95.csv",
+                            p_delimiter=",",
+                            p_frame=False,
+                            p_header=True,
+                            p_list_features=['PC1', 'PC2', 'PC3', 'PC4', 'PC5', 'PC6', 'PC7', 'PC8', 'PC9', 'PC10', 'PC11', 'PC12', 'PC13', 'PC14', 'PC15', 'PC16', 'PC17', 'PC18', 'PC19', 'PC20', 'PC21', 'PC22', 'PC23'],
+                            p_list_labels=['Label'])
+
+        tp_start = datetime.now()
+        myiterator = iter(stream)
+        print('Features:', stream.get_feature_space().get_num_dim(),
+              ', Labels:', stream.get_label_space().get_num_dim(),
+              ', Instances:', stream.get_num_instances() )
+        for i, curr_instance in enumerate(myiterator):
+            curr_data = curr_instance.get_feature_data().get_values()
 
 
-        # 1.2 Set up a stream workflow
+        # 3.1 Creation of a workflow
+        workflow = OAWorkflow(p_name='Input Signal',
+                              p_range_max=OAWorkflow.C_RANGE_NONE,
+                              p_ada=p_ada,
+                              p_visualize=p_visualize, 
+                              p_logging=p_logging)
+                              
 
-        # 1.2.1 Creation of a workflow
-        workflow = OAWorkflow( p_name='Anomaly Detection',
-                               p_range_max=OAWorkflow.C_RANGE_NONE,
-                               p_ada=p_ada,
-                               p_visualize=p_visualize,
-                               p_logging=p_logging )
+        # 3.2 Rearranger to reduce the number of features
+        features = stream.get_feature_space().get_dims()
+        features_new = [('F', features[1:3])]      
+        #features_new = [('F', [features[3], features[4], features[27], features[29]])]
 
+        task_rearranger = Rearranger(p_name='T1 - Rearranger',
+                                     p_range_max=Task.C_RANGE_NONE,
+                                     p_visualize=p_visualize,
+                                     p_logging=p_logging,
+                                     p_features_new=features_new)
 
-        # 1.2.2 Creation of tasks and add them to the workflow
+        workflow.add_task(p_task=task_rearranger)
 
         # Cluster Analyzer
         task_clusterer = SPARCCStream( p_name = 'SPARCCStream',
@@ -67,12 +85,12 @@ class MyScenario(OAScenario):
                                        p_logging =  p_logging )
 
 
-        workflow.add_task(p_task = task_clusterer)
+        workflow.add_task(p_task = task_clusterer, p_pred_tasks=[task_rearranger])
 
         # Anomaly Detector
         task_anomaly_detector = ClusterDriftDetector(p_clusterer=task_clusterer,
                                                      p_with_time_calculation=False,
-                                                     p_velocity_threshold_factor=2,
+                                                     p_velocity_threshold_factor=3,
                                                      p_initial_skip=300,
                                                      p_buffer_size=10,
                                                      p_visualize=p_visualize,
@@ -80,40 +98,35 @@ class MyScenario(OAScenario):
 
         workflow.add_task(p_task=task_anomaly_detector, p_pred_tasks=[task_clusterer])
 
-        # 1.3 Return stream and workflow
+        # 4 Return stream and workflow
         return stream, workflow
 
-
-
-# 2 Prepare Demo/Unit test mode
+# 1 Preparation of demo/unit test mode
 if __name__ == '__main__':
     cycle_limit = 1000
-    #logging     = Log.C_LOG_NOTHING
-    logging     = Log.C_LOG_ALL
-    visualize   = True
-    step_rate   = 1
+    logging = Log.C_LOG_ALL
+    visualize = True
+    step_rate = 1
 else:
     cycle_limit = 2
-    logging     = Log.C_LOG_NOTHING
-    visualize   = False
-    step_rate   = 1
+    logging = Log.C_LOG_NOTHING
+    visualize = False
+    step_rate = 1
 
+# 2 Instantiate the stream scenario
+myscenario = MyScenario(p_mode=Mode.C_MODE_SIM,
+                        p_cycle_limit=cycle_limit,
+                        p_visualize=visualize,
+                        p_logging=logging)
 
-
-# 3 Instantiate the stream scenario
-myscenario = MyScenario( p_mode=Mode.C_MODE_SIM,
-                               p_cycle_limit=cycle_limit,
-                               p_visualize=visualize,
-                               p_logging=logging )
-
-
-# 4 Reset and run own stream scenario
+# 3 Reset and run own stream scenario
 myscenario.reset()
 
 if __name__ == '__main__':
-    myscenario.init_plot( p_plot_settings=PlotSettings( p_view = PlotSettings.C_VIEW_ND,
-                                                        p_step_rate = step_rate ) )
+    myscenario.init_plot(p_plot_settings=PlotSettings(p_view=PlotSettings.C_VIEW_ND,
+                                                      p_step_rate=step_rate))
     input('\nPlease arrange all windows and press ENTER to start stream processing...')
+
 
 tp_before           = datetime.now()
 myscenario.run()
@@ -125,7 +138,7 @@ myscenario.log(Log.C_LOG_TYPE_W, 'Duration [sec]:', round(duraction_sec,2), ', C
 
 
 # 5 Summary
-anomalies         = myscenario.get_workflow()._tasks[1].get_anomalies()
+anomalies         = myscenario.get_workflow()._tasks[2].get_anomalies()
 detected_anomalies= len(anomalies)
 
 myscenario.log(Log.C_LOG_TYPE_W, '-------------------------------------------------------')
@@ -141,8 +154,6 @@ for anomaly in anomalies.values():
      for x in clusters.keys():
         clusters_affected[x] = {}
         clusters_affected[x]["centroid"] = list(clusters[x].centroid.value)
-        clusters_affected[x]["size"] = clusters[x].size.value
-        clusters_affected[x]["age"] = clusters[x].age.value
      
      inst = anomaly.get_instances()[-1].get_id()
      myscenario.log(Log.C_LOG_TYPE_W, 
